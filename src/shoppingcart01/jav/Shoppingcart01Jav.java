75fe672de428723b4d51a2352d277ec4a28a64bc
Aluno <Aluno@10.85.198.36>
2024-06-06 14:19:08 -0300

    01

---
+++ src/shoppingcart01/jav/Shoppingcart01Jav.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package shoppingcart01.jav;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Shoppingcart01Jav {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+        
+        
+        
+        
+        // Assign the message variable 
+        
+        
+        // Print and run the code
+        
+    }
+
+        public ShoppingCart01() {
+        }
+}
+}
